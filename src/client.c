a97f623e4ddd7fbe1172e17c43a7254e2599a7bc
Sid <sid@ubnt.com>
2014-04-17 14:50:48 +0800

    client

---
+++ src/client.c
@@ -0,0 +1,50 @@
+#include <stdlib.h>//exit
+#include <stdio.h>//fprintf
+#include <sys/socket.h>//socket
+#include <netinet/in.h>//IPPROTO_UDP
+#include <netdb.h>//gethostbyname
+#include <string.h>//memset
+#include <arpa/inet.h>//inet_addr
+
+int
+main(int argc, char *argv[]) {
+  if (argc < 3) {
+    fprintf(stderr, "Usage: %s ip port\n", argv[0]);
+    exit(1);
+  }
+
+  int sock;
+  char *server_ip;
+  unsigned short server_port;
+  struct sockaddr_in cliAddr, serAddr;
+
+  if ((sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0)  {
+    fprintf(stderr, "socket failed\n");
+    exit(1);
+  }
+
+  server_ip = argv[2];
+  server_port = atoi(argv[3]);
+
+  memset(&cliAddr, 0, sizeof(cliAddr));
+  cliAddr.sin_family = AF_INET;
+  cliAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+  cliAddr.sin_port = htons(21152);
+
+  if (bind(sock, (struct sockaddr *) &cliAddr, sizeof(struct sockaddr)) < 0) {
+    fprintf(stderr, "bind failed\n");
+    exit(1);
+  }
+
+  memset(&serAddr, 0, sizeof(serAddr));
+  serAddr.sin_family = AF_INET;
+  serAddr.sin_addr.s_addr = inet_addr(server_ip);
+  serAddr.sin_port = htons(server_port);
+
+  if (sendto(sock, "hello", 5, 0, (struct sockaddr *) (&serAddr), sizeof(serAddr)) == -1) {
+    fprintf(stderr, "sendto failed\n");
+    exit(1);
+  }
+
+  exit(0);
+}
