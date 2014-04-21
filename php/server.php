805c82f66b2a0058ee0e4ace0a3ecc7f816fe10c
ubnt <cn@ubnt.com>
2014-04-21 11:44:55 +0800

    change php src

--- php/server.php
+++ php/server.php
@@ -2,7 +2,7 @@
 
 $host = '0.0.0.0';
 //$host = '121.52.238.170';
-$port = 50403;
+$port = 21151;
 
 $socket = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP) or die("Could not create
 socket\n");
@@ -15,3 +15,7 @@ $port = 0;
 socket_recvfrom($socket, $buf, 12, 0, $from, $port);
 
 echo "Received $buf from remote address $from and remote port $port";
+
+$socket2 = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
+$msg = 'hhhh';
+socket_sendto($socket2, $msg, strlen($msg), 0, $from, $port);
