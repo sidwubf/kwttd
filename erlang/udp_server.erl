b21bc855998d6542f5510a2ed45e93d2cfd957d3
jensonwu <wubofengwbf@gmail.com>
2014-05-31 13:12:24 +0800

    add erlang server demo

---
+++ erlang/udp_server.erl
@@ -0,0 +1,19 @@
+-module(udp_server).
+-export([start/0]).
+
+start() ->
+	spawn(fun() -> server(21151) end).
+	
+server(Port) ->
+	{ok, Socket} = gen_udp:open(Port, [binary, {active, false}]),
+	io:format("socket:~p~n", [Socket]),
+	loop(Socket).
+	
+loop(Socket) ->
+	inet:setopts(Socket, [{active, once}]),
+	receive
+		{udp, Socket, Host, Port, Bin} ->
+			io:format("received:~p~n", [Bin]),
+			gen_udp:send(Socket, Host, Port, Bin),
+			loop(Socket)
+	end.
